b1eb1597c52334cde227da326c31402b48f1f6d4
patelsaheb1219 <kaumik1219@gmail.com>
2018-02-11 23:08:19 +0530

    FrontEnd

---
+++ venv/app/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article one',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias vitae eos eius delectus id tempora inventore tenetur voluptates accusamus sunt, nesciunt voluptate, adipisci facere saepe deserunt minima, eaque optio. Unde.',
+            'author': 'Kishan Patel',
+            'create_date': '11-02-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias vitae eos eius delectus id tempora inventore tenetur voluptates accusamus sunt, nesciunt voluptate, adipisci facere saepe deserunt minima, eaque optio. Unde.',
+            'author': 'Freddy Thobhani',
+            'create_date': '11-02-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias vitae eos eius delectus id tempora inventore tenetur voluptates accusamus sunt, nesciunt voluptate, adipisci facere saepe deserunt minima, eaque optio. Unde.',
+            'author': 'Dhritesh Patel',
+            'create_date': '11-02-2018'
+        }
+    ]
+    return articles
